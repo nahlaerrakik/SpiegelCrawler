41483e51cfd07fea3764ecdbc24e22036cc2409a
github nana <nahlaerrakik@users.noreply.github.com>
2021-07-04 21:17:04 +0100

    Update run.py

--- run.py
+++ run.py
@@ -10,18 +10,17 @@ from crawler.spiders.articles import ArticleSpider
 
 async def main(task):
     while True:
-        await asyncio.sleep(5)
-        print("Starting crawling", datetime.now())
+        await asyncio.sleep(15 * 60)
+        print("Started crawling", datetime.now())
         task()
 
 
 if __name__ == '__main__':
     def crawl(): os.system('scrapy crawl {}'.format(ArticleSpider.name))
-
-
-    """thread = Thread(target=crawl)
-    print("Starting crawling", datetime.now())
-    thread.start()"""
+        
+    thread = Thread(target=crawl)
+    print("Started crawling", datetime.now())
+    thread.start()
 
     loop = asyncio.get_event_loop()
     tasks = [asyncio.ensure_future(main(crawl))]
